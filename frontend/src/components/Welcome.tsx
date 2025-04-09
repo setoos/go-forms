da51481fb61e1ef118705de7f5a9f0f478af59dc
Vrushali-Pawar18 <vrushalipawar1802@gmail.com>
2025-04-09 19:54:08 +0530

    changed footer height

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -577,7 +577,7 @@ function Welcome() {
         </section>
 
       </div>
-      <footer className="bg-white py-12 border-t border-gray-200">
+      <footer className="bg-white border-t border-gray-200">
         <div className="container mx-auto">
           <div className="flex flex-col md:flex-row justify-between items-center gap-8">
             <div className="flex items-center gap-2">
